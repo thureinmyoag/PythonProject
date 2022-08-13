1349ff127155277e18ab9c5f9efd6301f1f05b3f
Thureinmyoag <tyma1996@gmail.com>
2022-08-13 21:23:13 +0630

    create templates folder and add index html

--- mysite/gallery/urls.py
+++ mysite/gallery/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
+]
 
+
+urlpatterns += [
+    path('artists', views.artists, name='artists'),
 ]
--- mysite/gallery/views.py
+++ mysite/gallery/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+    """View function for home page of site."""
+
+def artists(request):
+    return render(request, 'artists.html')
+
+    
