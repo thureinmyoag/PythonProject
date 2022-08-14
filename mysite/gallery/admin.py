ac105b8a6b359234685889b39e6202fd2e9b14b6
Thureinmyoag <tyma1996@gmail.com>
2022-08-14 21:21:04 +0630

    create arts genre, artist model

--- mysite/gallery/admin.py
+++ mysite/gallery/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Genre,Arts,Artist
+
+admin.site.register(Genre)
+admin.site.register(Arts)
+admin.site.register(Artist)
--- mysite/gallery/models.py
+++ mysite/gallery/models.py
@@ -1,3 +1,59 @@
 from django.db import models
 
 # Create your models here.
+class Genre(models.Model):
+    """Model representing a book genre."""
+    name = models.CharField(max_length=200, help_text='Enter an arts genre (e.g. painting)')
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.name
+
+from django.urls import reverse # Used to generate URLs by reversing the URL patterns
+
+class Arts(models.Model):
+    """Model representing a book (but not a specific copy of a book)."""
+    title = models.CharField(max_length=200)
+
+    # Foreign Key used because book can only have one author, but authors can have multiple books
+    # Author as a string rather than object because it hasn't been declared yet in the file
+    artist = models.ForeignKey('Artist', on_delete=models.SET_NULL, null=True)
+
+    summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
+
+    image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
+
+    isbn = models.CharField('ISBN', max_length=13, unique=True,
+                             help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+
+    # ManyToManyField used because genre can contain many books. Books can cover many genres.
+    # Genre class has already been defined so we can specify the object above.
+    genre = models.ManyToManyField(Genre, help_text='Select a genre for this artwork')
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.title
+
+    def get_absolute_url(self):
+        """Returns the URL to access a detail record for this book."""
+        return reverse('book-detail', args=[str(self.id)])
+
+class Artist(models.Model):
+    """Model representing an author."""
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def get_absolute_url(self):
+        """Returns the URL to access a particular author instance."""
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return f'{self.last_name}, {self.first_name}'
+
+
